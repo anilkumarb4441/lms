d52094a9a08e981b1755823825f3aa93333790a9
nirmalrajana <nirmal.sai@verzeo.com>
2022-06-22 17:32:24 +0530

    addLeadForm

---
+++ src/components/addEditLeadForm/addEditLeadForm.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./addEditLeadForm.css"
+import Modal from "../modal/modal"
+import Input from "../Input"
+
+function addEditLeadForm({show,handleDisplay,submitForm,handleInputChange,formData}) {
+    return (
+        <Modal
+      header={false}
+      modalClass="addChefModal"
+      show={show}
+      bodyClick = {true}
+      handleDisplay={(e)=>handleDisplay(e)}
+      body = { <div className = 'addLead'>
+          <div className = 'addLeadCol1'>
+              <p>Add New Chef</p>
+              <form onSubmit = {(e)=>{submitForm(e)}}>
+              <div>
+              {formData &&
+            formData.map((item, i) => {
+              return (
+                <Input {...item} required = {true} inputClass = 'addLeadInput' key={i}  change={(e) => handleInputChange(e, i)} />
+              );
+            })}
+              </div>
+              <button className = 'saveBtn' type='submit'>Save</button>
+              </form>
+          </div>
+      </div>
+  }
+      />
+    )
+}
+
+export default addEditLeadForm
