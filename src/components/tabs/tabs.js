05e7e17e758b349ed53c51c6c8ab33b949acee94
nirmalrajana <nirmal.sai@verzeo.com>
2022-06-23 11:19:21 +0530

    addLeadForm

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-function Tabs({tabArr}) {
+function Tabs({tabArr,handleTab}) {
+
+    const onclick = (item)=>{
+       handleTab(item)
+    }
     return (
         <div>
             {tabArr && tabArr.map((item,i)=>{
-               return <button>
-               </button>
+               return <button key = {i} onClick = {()=>onclick(item)}>{item}</button>
             })}
         </div>
     )
