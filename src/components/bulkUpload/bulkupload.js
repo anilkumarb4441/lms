0a459611ecbe7f04a7c86535512022e348d3f44d
Manojkumar0785 <manojkumar.o@verzeo.com>
2022-06-28 16:33:24 +0530

    Bulk

--- src/components/bulkUpload/bulkupload.js
+++ src/components/bulkUpload/bulkupload.js
@@ -4,6 +4,10 @@ import Modal from "../modal/modal";
 import Input from "../Input";
 import xlsxParser from 'xlsx-parse-json';
 import exportFromJSON from 'export-from-json';
+import axios from "axios";
+
+// Assets
+import close from "../../assets/Bulkupload/close.svg"
 
 function BulkUpload({
   show,
@@ -18,8 +22,40 @@ function BulkUpload({
     const [filename, setfilename] = useState("");
     const[errorfile, seterrorfile] = useState(false)
 
+    function downloadCSV(){
+      var csv = 'employeeid,status,date_of_leaving,leftTheOrganization,reason\n';
+      var data = [
+          {employeeid:'VZEOXXX', status:'AWOL', date_of_leaving:'2022-05-11', leftTheOrganization:false,reason:"Reason Here"},
+          {employeeid:'VZEOXXX', status:'SICK', date_of_leaving:'2022-05-14', leftTheOrganization:false,reason:"Reason Here"},
+          {employeeid:'VZEOXXX', status:'AWOL', date_of_leaving:'2022-02-11', leftTheOrganization:false,reason:"Reason Here"},
+          {employeeid:'VZEOXXX', status:'SICK', date_of_leaving:'2022-01-11', leftTheOrganization:true,reason:"Reason Here"},
+          {employeeid:'VZEOXXX', status:'AWOL', date_of_leaving:'2022-03-01', leftTheOrganization:true,reason:"Reason Here"},
+          {employeeid:'VZEOXXX', status:'AWOL', date_of_leaving:'2022-04-08', leftTheOrganization:false,reason:"Reason Here"},
+      ];
+      const fileName = 'sheet1'
+      const exportType = 'csv'
+      exportFromJSON({ data,fileName,exportType });
+   }
+
     function sendBulkData(){
-        console.log(sheetData,bulkData);
+        // console.log(sheetData,bulkData);
+        // let data = {sheetData:sheetData,source:bulkData.source}
+        // console.log(data,fosrmref.current);
+        // formref.current.value="";
+        console.log(bulkData);
+        // axios({
+        //     method:'post',
+        //     url:URLS.getAutoSearchData,
+        //     data:{data:text}
+        // }).then((res)=>{
+        //     if(res.status===200){
+        //         console.log(res)
+        //         let data = res?.data?.directMembers?res.data.directMembers:[]
+        //       setSuggestions(data)
+        //     }
+        // }).catch((err)=>{
+        //   setSuggestions([])
+        // }) 
     }
 
     const checkForEmptyValues = (data) => {
@@ -36,11 +72,11 @@ function BulkUpload({
     };
 
     const handleFile = (e) => {
-        console.log(e.target.files[0].name,"Entered");
         setfilename(e.target.files[0].name)
         if (e.target.files[0])
             xlsxParser.onFileSelection(e.target.files[0]).then((data) => {
                 sheetData = data['Sheet1'];
+                setbulkData({...bulkData,["data"]:sheetData})
                 if (!checkForEmptyValues(sheetData)) {
                     alert("File contains invalid data")
                     // setErrorMsg('File contains invalid data');
@@ -82,9 +118,17 @@ function BulkUpload({
                             style={{ display: 'none' }}
                             accept=".xlsx,.csv,.xls"
                         />
-                <button type="button" onClick={(e)=>formref.current.click()} className="BtnBulk">
-                {filename===""?"Upload File":filename}
-                </button>
+                        <div className="btnCrosParent">
+                          <button type="button" onClick={(e)=>formref.current.click()} className="BtnBulk">
+                          {filename===""?"Upload File":filename}
+                          </button>
+                          {
+                            filename!=""?<img src={close} alt="Close SVG" onClick={(e)=>{
+                            setfilename("");
+                            formref.current.value="";
+                            setbulkData({...bulkData,["data"]:""})
+                          }} />:null}
+                        </div>
                 {
                     filename!=""?
                     <select name="source" onChange={(e)=>setbulkData({...bulkData,[e.target.name]:e.target.value})}>
@@ -96,8 +140,8 @@ function BulkUpload({
                 }
               </div>
               {
-                errorfile===false && bulkData.source!="" ?
-                    <button className="saveBtn" type="button" onClick={sendBulkData()}>
+                errorfile===false && bulkData.source!="" && bulkData.data!="" ?
+                    <button className="saveBtn" type="button" onClick={(e)=>sendBulkData()}>
                         Upload
                     </button>
                     :null
