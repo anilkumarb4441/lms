468c41fa4a41aa5ead504ae0876be779d3e5c60e
nirmalrajana <nirmal.sai@verzeo.com>
2022-07-18 12:14:28 +0530

    removed unwanted components

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,11 +1,9 @@
 import {combineReducers} from 'redux'
 import leadsReducer from "./screens/leads/reducer"
-import servicesReducer from "./screens/services/reducer"
 import myTeamReducer from "./screens/Myteam/reducer"
 
 const rootReducer = combineReducers({
     leads:leadsReducer,
-    services:servicesReducer,
     myTeam:myTeamReducer
 })
 
