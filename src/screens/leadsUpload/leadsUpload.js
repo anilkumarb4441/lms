54a4d799d7e351105046fca85ac983eeb83203fe
anilkuamarverzeo <anil.kumar@verzeo.com>
2022-09-03 18:13:57 +0530

    tabel header options

--- src/screens/leadsUpload/leadsUpload.js
+++ src/screens/leadsUpload/leadsUpload.js
@@ -398,7 +398,7 @@ function LeadsUpload() {
               columns={[...columns]}
               data={tableData}
               tClass="bulkTable"
-              usId='627906f71f94140a082ef297'
+              usId={userId ==='627906f71f94140a082ef297'?'627906f71f94140a082ef297':''}
             />
           </div>
 
